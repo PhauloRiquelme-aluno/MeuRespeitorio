db430e8fd379eb35168d0e520a1fd4f2e4b96407
PhauloRiquelme-aluno <00001127937376sp@al.educacao.sp.gov.br>
2024-08-15 21:29:37 -0300

    Bug corrigido no sketch.js

--- sketch.js
+++ sketch.js
@@ -5,14 +5,20 @@ function setup() {
 
 let oldx = 0;
 let oldy = 0;
+let start = false;
 
 function draw() {
   stroke(0,0,255,100);
   fill(255,random(0,25),random(0,25));
   if(mouseIsPressed)
   {
+    if (!start) {
+      oldx = mouseX;
+      oldy = mouseY;
+    }
     line(mouseX,mouseY,oldx,oldy);
   }
+  start = mouseIsPressed;
   oldx = mouseX;
   oldy = mouseY;
 }
